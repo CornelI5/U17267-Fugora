1bb82ee7aea564e6cdbab7780a85ddbc887da8ea
CornelI5 <kenzoyooo0@gmail.com>
2026-07-25 21:21:46 +0700

    Update vector.py

--- Fugora/vector.py
+++ Fugora/vector.py
@@ -1,6 +1,5 @@
 import math
 
-
 class Vec3:
     __slots__ = ("x", "y", "z")
 
@@ -18,44 +17,8 @@ class Vec3:
     def __mul__(self, scalar):
         return Vec3(self.x * scalar, self.y * scalar, self.z * scalar)
 
-    def __rmul__(self, scalar):
-        return self.__mul__(scalar)
-
-    def __truediv__(self, scalar):
-        if scalar == 0:
-            return Vec3(0, 0, 0)
-        return Vec3(self.x / scalar, self.y / scalar, self.z / scalar)
-
-    def __neg__(self):
-        return Vec3(-self.x, -self.y, -self.z)
-
-    def __repr__(self):
-        return f"Vec3({self.x:.6e}, {self.y:.6e}, {self.z:.6e})"
-
-    def magnitude(self):
-        return math.sqrt(self.x ** 2 + self.y ** 2 + self.z ** 2)
-
     def magnitude_sq(self):
-        return self.x ** 2 + self.y ** 2 + self.z ** 2
-
-    def normalize(self):
-        mag = self.magnitude()
-        if mag == 0:
-            return Vec3(0, 0, 0)
-        return self / mag
-
-    def dot(self, other):
-        return self.x * other.x + self.y * other.y + self.z * other.z
-
-    def cross(self, other):
-        return Vec3(
-            self.y * other.z - self.z * other.y,
-            self.z * other.x - self.x * other.z,
-            self.x * other.y - self.y * other.x,
-        )
-
-    def distance_to(self, other):
-        return (self - other).magnitude()
+        return self.x**2 + self.y**2 + self.z**2
 
     def copy(self):
         return Vec3(self.x, self.y, self.z)
