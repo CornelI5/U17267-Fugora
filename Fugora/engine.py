84ae6e6f9539b7627e0ea564bbff27e8c251586d
CornelI5 <kenzoyooo0@gmail.com>
2026-07-25 15:34:12 +0700

    Update engine.py

--- Fugora/engine.py
+++ Fugora/engine.py
@@ -3,6 +3,8 @@ from .constants import DEFAULT_DT, SUN_MASS
 from .integrator import VelocityVerlet
 from .gravity import compute_nbody_gravity, detect_anomalies
 from .objects import CelestialObject
+from .events import EventManager
+from .sources import SourceManager
 
 
 class FugoraEngine:
@@ -15,14 +17,20 @@ class FugoraEngine:
         self.anomalies = []
         self.central_mass = SUN_MASS
         self._running = False
+        
+        # New components
+        self.events = EventManager()
+        self.sources = SourceManager()
 
     def add_object(self, obj):
         if not isinstance(obj, CelestialObject):
             raise TypeError("Expected CelestialObject instance")
         self.objects.append(obj)
+        self.events.emit("object_added", obj.id)
 
     def remove_object(self, obj_id):
         self.objects = [o for o in self.objects if o.id != obj_id]
+        self.events.emit("object_removed", obj_id)
 
     def get_object(self, obj_id):
         for obj in self.objects:
@@ -35,6 +43,8 @@ class FugoraEngine:
 
     def initialize(self):
         compute_nbody_gravity(self.objects)
+        self.sources.activate_all()
+        self.events.emit("engine_initialized")
 
     def step(self):
         self.integrator.step(self.objects)
@@ -44,7 +54,16 @@ class FugoraEngine:
         new_anomalies = detect_anomalies(
             self.objects, self.central_mass, self.time_elapsed
         )
-        self.anomalies.extend(new_anomalies)
+        
+        if new_anomalies:
+            self.anomalies.extend(new_anomalies)
+            self.events.emit("anomaly_detected", new_anomalies)
+
+        # Emit data update event
+        self.events.emit("step_completed", {
+            "time": self.time_elapsed,
+            "step": self.step_count
+        })
 
         return new_anomalies
 
@@ -61,7 +80,9 @@ class FugoraEngine:
             if callback:
                 callback(self, i, anomalies)
 
+        self.sources.deactivate_all()
         self._running = False
+        self.events.emit("simulation_finished")
 
     def stop(self):
         self._running = False
@@ -73,15 +94,17 @@ class FugoraEngine:
             "object_count": len(self.objects),
             "anomaly_count": len(self.anomalies),
             "objects": [obj.copy_state() for obj in self.objects],
+            "sources_active": list(self.sources.sources.keys()),
         }
 
     def summary(self):
         state = self.get_state()
         lines = [
-            f"FUGORA Engine Status",
+            f"FUGORA Engine v0.7 Status",
             f"  Time elapsed : {state['time_elapsed']:.2e} s",
             f"  Steps        : {state['step_count']}",
             f"  Objects      : {state['object_count']}",
             f"  Anomalies    : {state['anomaly_count']}",
+            f"  Sources      : {len(state['sources_active'])} active",
         ]
         return "\n".join(lines)
