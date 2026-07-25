7c7c9ba7001684bd66f12466fc496528873bf9ac
CornelI5 <kenzoyooo0@gmail.com>
2026-07-26 06:27:44 +0700

    Delete fugora directory

--- fugora/__init__.py
+++
@@ -1,7 +0,0 @@
-from .engine import FugoraEngine
-from .objects import CelestialObject, AnomalyRecord
-from .vector import Vec3
-from .visualization import OrbitVisualizer
-from .constants import G, AU, SUN_MASS, EARTH_MASS
-
-__version__ = "0.1.0"
