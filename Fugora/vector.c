b8764b4d9972a5b85c80cba90754477f23320c02
CornelI5 <kenzoyooo0@gmail.com>
2026-07-25 15:26:51 +0700

    Update vector.c

--- Fugora/vector.c
+++ Fugora/vector.c
@@ -1,25 +1,61 @@
 import math
 
-class Vec3:
-    def __init__(self, x=0.0, y=0.0, z=0.0):
-        self.x = x
-        self.y = y
-        self.z = z
 
-    def add(self, other):
+class Vec3:
+    __slots__ = ("x", "y", "z")
+
+    def __init__(self, x=0.0, y=0.0, z=0.0):
+        self.x = float(x)
+        self.y = float(y)
+        self.z = float(z)
+
+    def __add__(self, other):
         return Vec3(self.x + other.x, self.y + other.y, self.z + other.z)
 
-    def sub(self, other):
+    def __sub__(self, other):
         return Vec3(self.x - other.x, self.y - other.y, self.z - other.z)
 
-    def scale(self, s):
-        return Vec3(self.x * s, self.y * s, self.z * s)
+    def __mul__(self, scalar):
+        return Vec3(self.x * scalar, self.y * scalar, self.z * scalar)
+
+    def __rmul__(self, scalar):
+        return self.__mul__(scalar)
+
+    def __truediv__(self, scalar):
+        if scalar == 0:
+            return Vec3(0, 0, 0)
+        return Vec3(self.x / scalar, self.y / scalar, self.z / scalar)
+
+    def __neg__(self):
+        return Vec3(-self.x, -self.y, -self.z)
+
+    def __repr__(self):
+        return f"Vec3({self.x:.6e}, {self.y:.6e}, {self.z:.6e})"
 
     def magnitude(self):
-        return math.sqrt(self.x**2 + self.y**2 + self.z**2)
+        return math.sqrt(self.x ** 2 + self.y ** 2 + self.z ** 2)
+
+    def magnitude_sq(self):
+        return self.x ** 2 + self.y ** 2 + self.z ** 2
 
     def normalize(self):
         mag = self.magnitude()
         if mag == 0:
             return Vec3(0, 0, 0)
-        return self.scale(1.0 / mag)
+        return self / mag
+
+    def dot(self, other):
+        return self.x * other.x + self.y * other.y + self.z * other.z
+
+    def cross(self, other):
+        return Vec3(
+            self.y * other.z - self.z * other.y,
+            self.z * other.x - self.x * other.z,
+            self.x * other.y - self.y * other.x,
+        )
+
+    def distance_to(self, other):
+        return (self - other).magnitude()
+
+    def copy(self):
+        return Vec3(self.x, self.y, self.z)
